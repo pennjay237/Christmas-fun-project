7fe9a516480fc588632e1e9d9a9b1a25cddba9e4
pennjay237 <pennjude31@gmail.com>
2025-12-02 11:30:55 +0100

    add navigate to differenct pages

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
+import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
 
-
 export default function Header() {
-return (
-<header className={styles.header}>
-<h1 className={styles.title}>🎄 Christmas Wonderland 🎅</h1>
-</header>
-);
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.title}>🎄 Christmas Wonderland 🎅</h1>
+      <nav className={styles.nav}>
+        <Link to="/">Home</Link>
+        <Link to="/about">About Christmas</Link>
+        <Link to="/gifts">Gifts</Link>
+      </nav>
+    </header>
+  );
 }
